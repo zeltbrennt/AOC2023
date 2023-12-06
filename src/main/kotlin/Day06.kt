e1842c8e5bbd4a600e7e51bc4b95c8dea4294b83
zeltbrennt <zeltbrennt@gmail.com>
2023-12-06 08:26:20 +0100

    Optimize Day06

--- src/main/kotlin/Day06.kt
+++ src/main/kotlin/Day06.kt
@@ -1,23 +1,29 @@
 import util.loadAsList
+import kotlin.math.pow
+import kotlin.math.sqrt
 import kotlin.time.measureTime
 
 class Day06(
     private val input: List<String> = loadAsList(day = 6)
 ) {
 
-    fun calcRangeOfWays(time: Long, dist: Long): List<Int> =
-        LongRange(0, time).mapIndexed { i, v -> i to (time - v) * v }.filter { it.second > dist }.map { it.first }
+    fun calcRangeOfWays(time: Double, dist: Double): Int {
+        val low = (0.5 * (time - sqrt(time.pow(2) - 4 * dist))).toInt()
+        val up = (0.5 * (time + sqrt(time.pow(2) - 4 * dist))).toInt()
+        return up - low
+    }
+
 
     fun part1(): Int {
-        val times = Regex("\\d+").findAll(input[0]).map { it.groupValues[0] }.map { it.toLong() }.toList()
-        val dist = Regex("\\d+").findAll(input[1]).map { it.groupValues[0] }.map { it.toLong() }.toList()
-        return times.zip(dist).map { (t, d) -> calcRangeOfWays(t, d).size }.reduce { a, b -> a * b }
+        val times = Regex("\\d+").findAll(input[0]).map { it.groupValues[0] }.map { it.toDouble() }
+        val dist = Regex("\\d+").findAll(input[1]).map { it.groupValues[0] }.map { it.toDouble() }
+        return times.zip(dist).map { (t, d) -> calcRangeOfWays(t, d) }.reduce { a, b -> a * b }
     }
 
     fun part2(): Int {
-        val times = input[0].replace(" ", "").substringAfter(":").toLong()
-        val dist = input[1].replace(" ", "").substringAfter(":").toLong()
-        return calcRangeOfWays(times, dist).size
+        val times = input[0].replace(" ", "").substringAfter(":").toDouble()
+        val dist = input[1].replace(" ", "").substringAfter(":").toDouble()
+        return calcRangeOfWays(times, dist)
     }
 }
 
