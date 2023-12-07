73ef111eb9f45fa989b007a7d799b5b8396398ae
zeltbrennt <zeltbrennt@gmail.com>
2023-12-07 10:19:32 +0100

    Solve Part 2

--- src/main/kotlin/Day07.kt
+++ src/main/kotlin/Day07.kt
@@ -1,17 +1,21 @@
 import util.loadAsList
 import kotlin.time.measureTime
 
-enum class Card { N2, N3, N4, N5, N6, N7, N8, N9, T, J, Q, K, A }
+sealed interface Card
+enum class Order1 : Card { N2, N3, N4, N5, N6, N7, N8, N9, T, J, Q, K, A }
+enum class Order2 : Card { J, N2, N3, N4, N5, N6, N7, N8, N9, T, Q, K, A }
+
+enum class Rules { WITH_JOKER, WITHOUT_JOKER }
 
 enum class HandType { HIGH_CARD, ONE_PAIR, TWO_PAIR, THREE_OF_A_KIND, FULL_HOUSE, FOUR_OF_A_KIND, FIVE_OF_A_KIND }
 
 data class Bid(val hand: Hand, val value: Int)
 
-data class Hand(val cards: List<Card>) : Comparable<Hand> {
+data class Hand(val cards: List<Card>, val rules: Rules) : Comparable<Hand> {
 
     private val cardsByType = cards.groupBy { it }.mapValues { it.value.size }
 
-    val handType: HandType =
+    private val handType: HandType =
         if (cardsByType.size == 1) HandType.FIVE_OF_A_KIND
         else if (cardsByType.containsValue(4)) HandType.FOUR_OF_A_KIND
         else if (cardsByType.containsValue(3) && cardsByType.containsValue(2)) HandType.FULL_HOUSE
@@ -20,43 +24,60 @@ data class Hand(val cards: List<Card>) : Comparable<Hand> {
         else if (cardsByType.containsValue(2) && cardsByType.size == 4) HandType.ONE_PAIR
         else HandType.HIGH_CARD
 
+    fun getHandType(): HandType {
+        if (rules == Rules.WITHOUT_JOKER) return handType
+        val numJoker = cards.map { it.toString() == "J" }.count { it }
+        if (numJoker == 0) return handType
+        return when (handType) {
+            HandType.HIGH_CARD -> HandType.ONE_PAIR
+            HandType.ONE_PAIR -> HandType.THREE_OF_A_KIND
+            HandType.TWO_PAIR -> if (numJoker == 1) HandType.FULL_HOUSE else HandType.FOUR_OF_A_KIND
+            HandType.THREE_OF_A_KIND -> HandType.FOUR_OF_A_KIND
+            HandType.FULL_HOUSE -> HandType.FIVE_OF_A_KIND
+            HandType.FOUR_OF_A_KIND -> HandType.FIVE_OF_A_KIND
+            HandType.FIVE_OF_A_KIND -> HandType.FIVE_OF_A_KIND
+        }
+    }
+
     override fun compareTo(other: Hand): Int {
-        val a = this.handType
-        val b = other.handType
-        if (a != b) return a.compareTo(b)
-        this.cards.zip(other.cards).forEach { (t, o) ->
+        val thisHandType = this.getHandType()
+        val otherHandType = other.getHandType()
+        if (thisHandType != otherHandType) return thisHandType.compareTo(otherHandType)
+        val thisCardValues = cards.map { (it as Enum<*>).ordinal }
+        val otherCardValues = other.cards.map { (it as Enum<*>).ordinal }
+        thisCardValues.zip(otherCardValues).forEach { (t, o) ->
             if (t != o) return t.compareTo(o)
         }
         return 0
     }
 }
 
-fun String.toBid(): Bid {
+fun String.toBid(rules: Rules = Rules.WITHOUT_JOKER): Bid {
     val (handString, bid) = this.split(" ")
-    return Bid(handString.toHand(), bid.toInt())
+    return Bid(handString.toHand(rules), bid.toInt())
 }
 
-fun String.toHand(): Hand {
-    return Hand(this.map { it.toCard() })
+fun String.toHand(rules: Rules = Rules.WITHOUT_JOKER): Hand {
+    return Hand(this.map { it.toCard(rules) }, rules)
 }
 
-fun Char.toCard(): Card =
-    if (this.isDigit()) Card.valueOf("N$this") else Card.valueOf(this.toString())
+fun Char.toCard(ordering: Rules = Rules.WITHOUT_JOKER): Card {
+    return when (ordering) {
+        Rules.WITHOUT_JOKER -> if (this.isDigit()) Order1.valueOf("N$this") else Order1.valueOf(this.toString())
+        Rules.WITH_JOKER -> if (this.isDigit()) Order2.valueOf("N$this") else Order2.valueOf(this.toString())
+    }
+}
 
 
 class Day07(
-    input: List<String> = loadAsList(day = 7)
+    val input: List<String> = loadAsList(day = 7)
 ) {
 
-    private val bids = input.map { it.toBid() }
+    fun part1(): Int = input.map { it.toBid() }.calcTotalWinnings()
 
-    fun part1(): Int {
-        return bids.sortedBy { it.hand }.mapIndexed { i, b -> b.value * (i + 1) }.sum()
-    }
+    fun part2(): Int = input.map { it.toBid(Rules.WITH_JOKER) }.calcTotalWinnings()
 
-    fun part2(): Int {
-        TODO("Not yet implemented")
-    }
+    private fun List<Bid>.calcTotalWinnings() = this.sortedBy { it.hand }.mapIndexed { i, b -> b.value * (i + 1) }.sum()
 }
 
 fun main() {
--- src/test/kotlin/Day07Test.kt
+++ src/test/kotlin/Day07Test.kt
@@ -12,49 +12,49 @@ class Day07Test {
     @DisplayName("AAAAA is five of a kind")
     fun four_of_a_kind() {
         val hand = "AAAAA".toHand()
-        assertEquals(HandType.FIVE_OF_A_KIND, hand.handType)
+        assertEquals(HandType.FIVE_OF_A_KIND, hand.getHandType())
     }
 
     @Test
-    @DisplayName("AA8AA isfour of a kind")
+    @DisplayName("AA8AA is four of a kind")
     fun five_of_a_kind() {
         val hand = "AA8AA".toHand()
-        assertEquals(HandType.FOUR_OF_A_KIND, hand.handType)
+        assertEquals(HandType.FOUR_OF_A_KIND, hand.getHandType())
     }
 
     @Test
     @DisplayName("TTT98 is three of a kind")
     fun three_of_a_kind() {
         val hand = "TTT98".toHand()
-        assertEquals(HandType.THREE_OF_A_KIND, hand.handType)
+        assertEquals(HandType.THREE_OF_A_KIND, hand.getHandType())
     }
 
     @Test
     @DisplayName("23332 is full house")
     fun full_house() {
         val hand = "23332".toHand()
-        assertEquals(HandType.FULL_HOUSE, hand.handType)
+        assertEquals(HandType.FULL_HOUSE, hand.getHandType())
     }
 
     @Test
     @DisplayName("23432 is two pair")
     fun two_pair() {
         val hand = "23432".toHand()
-        assertEquals(HandType.TWO_PAIR, hand.handType)
+        assertEquals(HandType.TWO_PAIR, hand.getHandType())
     }
 
     @Test
     @DisplayName("A23A4 is one pair")
     fun one_pair() {
         val hand = "A23A4".toHand()
-        assertEquals(HandType.ONE_PAIR, hand.handType)
+        assertEquals(HandType.ONE_PAIR, hand.getHandType())
     }
 
     @Test
     @DisplayName("23456 is high card")
     fun high_card() {
         val hand = "23456".toHand()
-        assertEquals(HandType.HIGH_CARD, hand.handType)
+        assertEquals(HandType.HIGH_CARD, hand.getHandType())
     }
 
     @Test
@@ -84,7 +84,7 @@ class Day07Test {
     @DisplayName("Part 2")
     fun day07_part02_example01() {
         val solution = solver.part2()
-        assertEquals(0, solution)
+        assertEquals(5905, solution)
     }
 
 } 
