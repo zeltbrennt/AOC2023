aa29c011dbf76b90009d3f973eeaf246fd600290
zeltbrennt <zeltbrennt@gmail.com>
2023-12-12 17:31:12 +0100

    Rebuild the solution to use DP

--- src/main/kotlin/Day12.kt
+++ src/main/kotlin/Day12.kt
@@ -3,67 +3,67 @@ import kotlin.time.measureTime
 
 class Day12(private val input: List<String> = loadAsList(day = 12)) {
 
-    fun part1(): Int =
-        input.sumOf { getNumberOfArrangements(it) }
+    fun part1() = input.sumOf { getNumberOfArrangements(it) }
 
 
     fun part2(): Int {
         TODO("Not yet implemented")
     }
 
-    private fun templateToRegex(template: String): Regex =
-        template.replace(".", "!")
-            .replace("?", ".")
-            .replace("!", "\\.")
-            .toRegex()
-
-    private fun generateNumbers(numOfDigits: Int, base: Int): List<List<Int>> {
-        val result = mutableListOf<List<Int>>()
-        val combo = MutableList(numOfDigits) { 0 }
-        while (combo[0] < base) {
-            if (combo.windowed(2).all { (a, b) -> a <= b }) result.add(combo.toList())
-            combo[combo.lastIndex]++
-            for (i in combo.lastIndex downTo 1) {
-                if (combo[i] >= base) {
-                    combo[i] = 0
-                    combo[i - 1]++
-                }
-            }
-        }
-        return result
-    }
-
-    private fun getStartingIndices(input: List<Int>) =
-        input.runningFold(0) { acc, i -> acc + i }
-            .dropLast(1)
-            .mapIndexed { index, i -> i + index }
-
-    private fun getAllIndices(numOfDigits: Int, base: Int, input: List<Int>) =
-        generateNumbers(numOfDigits, base)
-            .map { getStartingIndices(input).zip(it).map { (a, b) -> a + b } }
-
-    private fun createIteration(length: Int, indices: List<Int>, groups: List<Int>): String {
-        var result = ".".repeat(length)
-        for ((i, len) in indices.zip(groups)) {
-            result = result.replaceRange(i, i + len, "#".repeat(len))
-        }
-        return result
-    }
-
-    fun getNumberOfArrangements(input: String): Int {
-        val (arr, temp) = input.split(" ")
-        val groups = temp.split(",").map { it.toInt() }
-        val base = arr.length - groups.sum() - groups.lastIndex + 1
-        val lengthOfString = arr.length
-        val regex = templateToRegex(arr)
-        return getAllIndices(groups.size, base, groups)
-            .map { createIteration(lengthOfString, it, groups) }
-            .count { it matches regex }
-    }
-
     fun unfold(input: String): String {
         val (arr, temp) = input.split(" ")
         return "${List(5) { arr }.joinToString("?")} ${List(5) { temp }.joinToString(",")}"
+    }
+
+    fun getNumberOfArrangements(input: String): Long {
+        val (springs, temp) = input.split(" ")
+        val groups = temp.split(",").map { it.toInt() }
+        return findCombinations(springs, groups)
+    }
+
+    private fun findCombinations(
+        springs: String,
+        target: List<Int>,
+        idx: Int = 0,
+        value: List<Int> = listOf(0),
+        debug: String = ""
+    ): Long {
+        if (idx > springs.lastIndex) {
+            return if (value.let { if (it.last() == 0) it.dropLast(1) else it } == target) 1
+            else 0
+        }
+        //TODO: add pruning for performance improvement
+        return when (springs[idx]) {
+            '#' -> findCombinations(
+                springs,
+                target,
+                idx + 1,
+                value.toMutableList().apply { this[this.lastIndex]++ }.toList(),
+                "$debug#"
+            )
+
+            '.' -> findCombinations(
+                springs,
+                target,
+                idx + 1,
+                if (value.last() == 0) value else value.toMutableList().apply { add(0) }.toList(),
+                "$debug."
+            )
+
+            else -> findCombinations(
+                springs,
+                target,
+                idx + 1,
+                value.toMutableList().apply { this[this.lastIndex]++ }.toList(),
+                "$debug#"
+            ) + findCombinations(
+                springs,
+                target,
+                idx + 1,
+                if (value.last() == 0) value else value.toMutableList().apply { add(0) }.toList(),
+                "$debug."
+            )
+        }
     }
 }
 
